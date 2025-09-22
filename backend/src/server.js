f0e3813ced06bbf11a8c8400fd324d0d0b43a110
ChaminduDKC <chamindud061@gmail.com>
2025-09-22 12:15:17 +0530

    start server function added

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,14 +2,24 @@
 // set type:"module" to use import export
 
 import express from 'express'
-import {ENV} from "./config/env.config.js";
-import {connectDB} from "./config/db.config.js";
+import { ENV } from "./config/env.config.js";
+import { connectDB } from "./config/db.config.js";
 
 const PORT = ENV.PORT;
 
 const app = express();
-connectDB().then(()=>{
-    app.listen(PORT, ()=> {
-        console.log(`Server is up and run on port ${PORT}`)
-    })
-});
+
+const startServer = async () => {
+    try {
+        await connectDB();
+        app.listen(PORT, () => {
+            console.log(`Server is up and run on port ${PORT}`)
+        })
+    } catch (error) {
+        logger.error("Error in starting server", error);
+        process.exit(1);
+    }
+
+}
+startServer();
+
